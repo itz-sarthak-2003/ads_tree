00e0b50bb137a3199820121fdbe88499bfc79512
itz-sarthak-2003 <128714788+itz-sarthak-2003@users.noreply.github.com>
2024-03-24 11:27:56 +0530

    Add files via upload

--- family_tree.cpp
+++ family_tree.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class FMT
 {
 public:
-    int age, bdate,c=0;
+    int age, bdate, c = 0;
     string name, gender;
     FMT *lchild, *rchild;
     void accept();
@@ -13,41 +13,44 @@ public:
     void display_postorder(FMT *root);
     void mirror(FMT *root);
     void calculate();
-    // void find_member(FMT *root,string x);
+    void find_member(FMT *root, string x);
 } *root;
 
-void FMT::calculate(){
-    cout<<"total no. of records : "<<c;
+void FMT::calculate()
+{
+    cout << "total no. of records : " << c;
 }
 
-// void FMT::find_member(FMT *root,string x){
-//             cout<<"enter name of family member u want to search : "<<endl;
-//         cin>>x;
-//     if(root==NULL){
-//         return;
-//     }
-//     if(root->name==x){
-//         cout<<"family member is found : "<<root->name;
-//         return(find_member(root->rchild,x));
-//         return(find_member(root->lchild,x));
-//     }
-//     else{
-//         cout<<"family member is not found "<<endl;
-//     }
-// }
+void FMT::find_member(FMT *root, string x)
+{
+    if (root == NULL)
+    {
+        cout << "family tree is empty";
+    }
+    if (root->name == x)
+    {
+        cout << "family member is found : " << root->name << endl;
+        return;
+    }
+    find_member(root->lchild, x);
+    find_member(root->rchild, x);
+}
 
-void FMT::mirror(FMT *root){
+void FMT::mirror(FMT *root)
+{
     FMT *temp;
-    if(root==NULL){
-        return;}
-        else{
+    if (root == NULL)
+    {
+        return;
+    }
+    else
+    {
         mirror(root->lchild);
         mirror(root->rchild);
-        temp=(root->lchild);
-        root->lchild=root->rchild;
-        root->rchild=temp;
+        temp = (root->lchild);
+        root->lchild = root->rchild;
+        root->rchild = temp;
     }
-
 }
 void FMT::display_preorder(FMT *root)
 {
@@ -57,7 +60,7 @@ void FMT::display_preorder(FMT *root)
     }
     else
     {
-        cout << "\n"<< root->name << "\t" << root->age << "\t" << root->gender << "\t" << root->bdate << endl;
+        cout << "\n" << root->name << "\t" << root->age << "\t" << root->gender << "\t" << root->bdate << endl;
         display_preorder(root->lchild);
         display_preorder(root->rchild);
     }
@@ -71,7 +74,7 @@ void FMT::display_inorder(FMT *root)
     else
     {
         display_inorder(root->lchild);
-        cout << "\n"<< root->name << "\t" << root->age << "\t" << root->gender << "\t" << root->bdate << endl;
+        cout << "\n" << root->name << "\t" << root->age << "\t" << root->gender << "\t" << root->bdate << endl;
         display_inorder(root->rchild);
     }
 }
@@ -85,7 +88,7 @@ void FMT::display_postorder(FMT *root)
     {
         display_postorder(root->lchild);
         display_postorder(root->rchild);
-        cout << "\n"<< root->name << "\t" << root->age << "\t" << root->gender << "\t" << root->bdate << endl;
+        cout << "\n" << root->name << "\t" << root->age << "\t" << root->gender << "\t" << root->bdate << endl;
     }
 }
 
@@ -119,12 +122,13 @@ void FMT::accept()
 void FMT::insert(FMT *root, FMT *next)
 {
     string chr;
-    cout << "where u want to inserted data either left or right of " <<"'"<< root->name <<"'" << " L or R : " << endl;
+    cout << "where u want to inserted data either left or right of "<< "'" << root->name << "'" << " L or R : " << endl;
     cin >> chr;
     if (chr == "l" || chr == "L")
     {
         if (root->lchild == NULL)
-        {c++;
+        {
+            c++;
             root->lchild = next;
         }
         else
@@ -135,7 +139,8 @@ void FMT::insert(FMT *root, FMT *next)
     if (chr == "r" || chr == "R")
     {
         if (root->rchild == NULL)
-        {c++;
+        {
+            c++;
             root->rchild = next;
         }
         else
@@ -147,15 +152,12 @@ void FMT::insert(FMT *root, FMT *next)
 int main()
 {
     FMT n;
-    // fmt.accept();
-    // fmt.display(root);
-
     int ch;
     string x;
     do
     {
         cout << "\nWELCOME TO FAMILY TREE ! ";
-        cout << "\n1.Accept the family members \n2.Display family members in PREORDER \n3.Display family members in INORDER \n4.Display family members in POSTORDER \n5.Display mirror image of ur family tree \n6.calculate how many members in the family \n7.exit";
+        cout << "\n1.Accept the family members \n2.Display family members in PREORDER \n3.Display family members in INORDER \n4.Display family members in POSTORDER \n5.Display mirror image of ur family tree \n6.calculate how many members in the family \n7. find family member \n8.exit";
         cout << "\nENTER YOUR CHOICE = : ";
         cin >> ch;
         switch (ch)
@@ -176,20 +178,24 @@ int main()
             n.display_postorder(root);
             break;
         case 5:
-            cout<<"for displaying mirror image of family tree u can choice preorder,inorder,postorder any of them "<<endl;
+            cout << "for displaying mirror image of family tree u can choice preorder,inorder,postorder any of them " << endl;
             n.mirror(root);
-            //n.display_inorder(root);
             break;
-            case 6:
+        case 6:
             n.calculate();
             break;
         case 7:
-             exit(0);
+            cout << "enter the name of the family member u want to search for : ";
+            cin >> x;
+            n.find_member(root, x);
+            break;
+        case 8:
+            exit(0);
             cout << "Thanks........";
             break;
         default:
             cout << "Wrong choice";
         }
-    } while (ch != 7);
+    } while (ch != 8);
     return 1;
 }
